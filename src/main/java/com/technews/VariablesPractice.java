625143832c8ab3502b227626443a0e3fbc1b62c9
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2023-01-04 14:20:19 -0500

    created user model

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int number = 10;
+        double dec = 1.99;
+        double sum = number + dec;
+
+        String hello = "hello" + number;
+
+        System.out.println(sum + " " + hello);
+
+
+    }
+}
